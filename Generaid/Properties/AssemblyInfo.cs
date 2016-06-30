cbb828512d39797905de499aedf6e814fe6997e1
marhoily <marhoily@gmail.com>
2016-06-30 11:49:45 +0300

    fix version

--- Generaid/Properties/AssemblyInfo.cs
+++ Generaid/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("7b227385-b83d-4e16-868e-e307967d0ee4")]
 
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: InternalsVisibleTo("Generaid.Tests")]
