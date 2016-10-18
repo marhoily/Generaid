a6d8311531b7b9662946a76046d0d465c23cc326
marhoily <marhoily@homtail.com>
2016-10-18 22:09:56 +0300

    + GenerateFileNamesFixer

---
+++ Generaid.Tests/FixerFacts.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using GeneratedFileNamesFixer;
+using Xunit;
+
+namespace Generaid
+{
+    public sealed class FixerFacts
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            Program.Fix("...<LastGenOutput>WrapperInterfaceHost.cs</LastGenOutput>...")
+                .Should().Be("...<LastGenOutput>WrapperInterfaceHost.g.cs</LastGenOutput>...");
+        }
+        [Fact]
+        public void FactMethodName2()
+        {
+            Program.Fix("...<LastGenOutput>WrapperInterfaceHost.cs")
+                .Should().Be("...<LastGenOutput>WrapperInterfaceHost.cs");
+        }
+        [Fact]
+        public void FactMethodName1()
+        {
+            Program.Fix("...")
+                .Should().Be("...");
+        }
+    }
+}
---
+++ GeneratedFileNamesFixer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+// ReSharper disable StringIndexOfIsCultureSpecific.3
+// ReSharper disable StringIndexOfIsCultureSpecific.2
+
+namespace GeneratedFileNamesFixer
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            var projectFiles = Directory.EnumerateFiles(
+                Environment.CurrentDirectory, "*.csproj");
+            foreach (var file in projectFiles)
+            {
+                var oldText = File.ReadAllText(file);
+                var newText = Fix(oldText);
+                if (oldText != newText)
+                    File.WriteAllText(file, newText);
+            }
+        }
+
+        public static string Fix(string input)
+        {
+            var sb = new StringBuilder();
+            var curr = 0;
+            var open = input.IndexOf("<LastGenOutput>", curr);
+            while (open != -1)
+            {
+                var close = input.IndexOf("</LastGenOutput>", open);
+                if (close != -1)
+                {
+                    if (input.IndexOf(".g.", open, close - open) == -1)
+                    {
+                        var dot = input.IndexOf(".", open, close - open);
+                        if (dot != -1)
+                        {
+                            sb.Append(input.Substring(curr, dot - curr));
+                            sb.Append(".g");
+                            curr = dot - curr;
+                        }
+                    }
+                    open = input.IndexOf("<LastGenOutput>", close);
+                }
+                else
+                {
+                    open = input.IndexOf("<LastGenOutput>", open+ "<LastGenOutput>".Length);
+                }
+            }
+            sb.Append(input.Substring(curr, input.Length - curr));
+            return sb.ToString();
+        }
+    }
+}
